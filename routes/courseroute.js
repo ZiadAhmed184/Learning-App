a3affcbc1833888349914f3eee15c162fd92ee20
Ziad Ahmed <ziadahmedshawky123@gmail.com>
2023-07-15 07:06:05 +0300

    name modification

--- routes/courseroute.js
+++ routes/courseroute.js
@@ -2,6 +2,8 @@ const express = require("express");
 const multer=require('multer')
 const path=require('path')
 const {
+  getCourseValidator,
+  creatCourseValidator,
   UpdateCourseValidator,
   DeleteCourseValidator,
 } = require("../utils/Validattors/CourseValidator");
@@ -12,7 +14,7 @@ const {
   getCourse,
   updateCourse,
   deleteCourse,
-} = require("../controllers/courseControler");
+} = require("../controllers/Coursecontroler");
 const authController = require("../controllers/authController");
 
 
@@ -28,7 +30,7 @@ const multerFilter=(req,file,cb)=>{
   if(file.mimetype.split("/")[1]==='mp4'){
     cb(null,true)
   }else{
-    cb(new Error('Please Insert a video'),false)
+    cb(new Error('Please a video'),false)
   }
 }
 
