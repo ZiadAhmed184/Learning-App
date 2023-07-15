fc66634dc60953a5ef003302011ef1d994ad9b2e
Ziad Ahmed <ziadahmedshawky123@gmail.com>
2023-07-15 06:57:23 +0300

    database connection

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,11 +3,6 @@ mongoose.set("strictQuery", true);
 
 const dbConnection = () => {
   mongoose.connect(
-    // "mongodb+srv://shrook:KSSl3g4bURgEFwxy@cluster0.kgf2ma6.mongodb.net/learnning_Website_DB?retryWrites=true&w=majority",
-    // {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true,
-    // }
     process.env.DB_URI
   );
 };
