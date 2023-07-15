1c6561f9345aafe903192811d1aaa60547967e15
Ziad Ahmed <ziadahmedshawky123@gmail.com>
2023-07-15 04:48:53 +0300

    star1234

--- public/js/login.js
+++ public/js/login.js
@@ -1,30 +1,38 @@
-const login=async(email,password)=>{
-    console.log(email,password);
- try {
-    const res=await axios({
-        method:'POST',
-        url:'http://localhost:3000/auth/login',
-        data:{
-            email,
-            password,
-        }
-     })
-
-     if(res.data.status==='success'){
-        location.assign('/')
+const login = async (email, password) => {
+    try {
+      const res = await axios({
+        method: 'POST',
+        url: 'http://localhost:3000/auth/login',
+        data: {
+          email,
+          password,
+        },
+      });
+  
+      if (res.data.status === 'success') {
+        location.assign('/');
+      } else {
+        throw new Error('Login failed');
+      }
+    } catch (error) {
+      if (error.response) {
+        // The request was made and the server responded with a non-2xx status code
+        alert(error.response.data.message);
+      } else if (error.request) {
+        // The request was made but no response was received
+        alert('No response received from the server');
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        alert('Error:', error.message);
+      }
     }
-     //window.location.href='/'
- } catch (error) {
-    alert(error.response.data.message);
- }
-}
-
-const formEl=document.querySelector('.login')
-formEl.addEventListener('submit',(e)=>{
+  };
+  
+  const formEl = document.querySelector('.login');
+  formEl.addEventListener('submit', (e) => {
     e.preventDefault();
-    const email=document.getElementById('email').value;
-    const password=document.getElementById('password').value;
-    login(email,password)
-
-})
-
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+  
